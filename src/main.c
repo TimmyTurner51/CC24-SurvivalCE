4cb663cc024851ccae6df3506a4d5baaa13486dd
s-silver <69227528+s-silver@users.noreply.github.com>
2020-10-07 11:23:01 -0700

    Initialize main.c
    Added template code.

--- src/main.c
+++ src/main.c
@@ -1,1 +1,41 @@
 
+////////////////////////////////////////
+// SurvivalCE
+// Authors: TimmyTurner62 and Michael0x18
+// License: ? Gnu GPL?
+// Description: CC24 Cemetech contest submission
+////////////////////////////////////////
+
+/* Keep these headers */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+
+/* Standard headers */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Other available headers: stdarg.h, setjmp.h, assert.h, ctype.h, float.h, iso646.h, limits.h, errno.h */
+
+/* uint8_t is an unsigned integer that can range from 0-255. */
+/* It performs faster than just an int, so try to use it (or int8_t) when possible */
+
+/* Put function prototypes here */
+void printText(const char *text, uint8_t x, uint8_t y);
+
+/* Put all your code here */
+void main(void){
+    //do nothing for now.
+    return;
+}
+
+/* Draw text on the homescreen at the given X/Y cursor location */
+void printText(const char *text, uint8_t xpos, uint8_t ypos){
+    os_SetCursorPos(ypos, xpos);
+    os_PutStrFull(text);
+}
+
+
