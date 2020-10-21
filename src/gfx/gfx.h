061a52b23383ec31f2807df513ac2395c616b208
TimmyTurner51 <46533729+TimmyTurner51@users.noreply.github.com>
2020-10-21 10:58:59 -0500

    Made it properly *hopefully*

--- src/gfx/gfx.h
+++ src/gfx/gfx.h
@@ -1,15 +1,15 @@
-#ifndef gfx_include_file
-#define gfx_include_file
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "global_palette.h"
-#include "tileset.h"
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+#ifndef gfx_include_file
+#define gfx_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mypalette.h"
+#include "tileset.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
